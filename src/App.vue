f12f079a40f6754a435ff9db8ed9954a4f68e901
jin.fang <jin.fang@advance.ai>
2021-06-29 16:38:15 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <a-row>
+    <a-col :span="6">col-6</a-col>
+    <a-col :span="6">col-6</a-col>
+    <a-col :span="6">col-6</a-col>
+    <a-col :span="6">col-6</a-col>
+  </a-row>
+</template>
+
+<script setup>
+// import { Row,Col } from 'antd';
+// console.log(Row);
+// This starter template is using Vue 3 experimental <script setup> SFCs
+// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
